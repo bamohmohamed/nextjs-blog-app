e8c52fd98eed6098560ee407eea71b16f0cac49a
Mohamed Bamoh <m.bamoh@pharmatrace.io>
2022-11-21 19:44:50 +0100

    finalizing post content detail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,70 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import DUMMY_POST from "../../../dummy/post-data";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nord } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.properties.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={nord}
+          language={language}
+          // children={children}
+        >
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          p: customRenderers.p,
+          code: customRenderers.code,
+        }}
+      >
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
--- markdown/nextjs-in-action.md
+++ markdown/nextjs-in-action.md
@@ -3,8 +3,32 @@ slug: nextjs-in-action
 title: "NextJs in Action"
 date: "2022-11-17"
 image: nextjs-in-action.png
-excerpt: "Enter Next.js, the React Framework. Next.js provides a solution to all of the above problems."
+excerpt: NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
 isFeatured: true
 ---
 
-# This post will show Next js in Action
+NextJS is a **framework for ReactJS**.
+
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
+
+Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people.
+
+Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
+
+## Why would we then need NextJS?
+
+Because NextJS makes building React apps easier - especially React apps that should have server-side rendering (though it does way more than just take care of that).
+
+In this article, we'll dive into the core concepts and features NextJS has to offer:
+
+- File-based Routing
+- Built-in Page Pre-rendering
+- Rich Data Fetching Capabilities
+- Image Optimization
+- Much More
+
+## File-based Routing
+
+![Create routes via your file + folder structure](nextjs-file-based-routing.png)
+
+... More content ...
